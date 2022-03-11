663fe0eccc8fb7a28f658f563ab615f110c40433
Kristopher Lam <root@sandb0x.io>
2022-03-11 13:02:37 +0800

    add try catch block to submission api

--- pages/api/submissions/index.ts
+++ pages/api/submissions/index.ts
@@ -57,76 +57,83 @@ export default async function (req, res) {
       encoding: 'utf-8'
     });
     form.parse(req, async (err, fields, { files }) => {
-      if(err) {
-        throw err;
-      } else {
-        if(Array.isArray(files)) {
-          if(files.filter(({ name, hash }) => hash!==fields[`checksum;${name}`]).length > 1) {
-            throw new Error('One or more checksum mismatched, potential transmission corruption detected');
-          }
-          const zip = new AdmZip();
-          for (const file of files) {
-            zip.addFile(file.name, readFileSync(file.path));
-          }
-          const filename = `${Date.now()}_${user}_aggregated.zip`
-          const destinationFilename = `submitted/${filename}`;
-          const savedPath = `${process.env.NEXT_PUBLIC_UPLOAD_DIR}/${destinationFilename}`
-          zip.writeZip(savedPath);
-          const { size } = statSync(savedPath);
-          const submission: Submission = {
-            stored_name: destinationFilename,
-            upload_name: filename,
-            assignment_config_id: parseInt(fields.assignmentConfigId, 10),
-            size,
-            checksum: sha256File(savedPath),
-            user_id: parseInt(user, 10)
-          }
-          await submit(req.headers.cookie, submission);
-          res.json({
-            status: 'success'
-          });
+      try {
+        if(err) {
+          throw err;
         } else {
-          if(files.hash!==fields[`checksum;${files.name}`]) {
-            return res.status(500).json({
-              status: 'error',
-              error: 'Checksum mismatched, potential transmission corruption detected'
-            });
-          }
-          const destinationFilename = `submitted/${files.lastModifiedDate.getTime()}_${user}_${files.path.replace(`/tmp/`, '')}`
-          const submission: Submission = {
-            stored_name: destinationFilename,
-            upload_name: files.name,
-            assignment_config_id: parseInt(fields.assignmentConfigId, 10),
-            size: files.size,
-            checksum: files.hash,
-            user_id: parseInt(user, 10)
-          }
-          try {
-            copyFile(files.path, `${process.env.NEXT_PUBLIC_UPLOAD_DIR}/${destinationFilename}`, async (err) => {
-              if(!err) {
-                const {id} = await submit(req.headers.cookie, submission);
-                return res.json({
-                  status: 'success',
-                  id
-                });
-              }
-            });
-          } catch (error: any) {
-            if (error.message.includes(`Cannot read property 'createSubmission' of undefined`)) {
-
-              return res.status(403).json({
-                status: 'error',
-                error: 'Timeline misalignment for requested submission'
-              })
+          if(Array.isArray(files)) {
+            if(files.filter(({ name, hash }) => hash!==fields[`checksum;${name}`]).length > 1) {
+              throw new Error('One or more checksum mismatched, potential transmission corruption detected');
             }
-            else {
+            const zip = new AdmZip();
+            for (const file of files) {
+              zip.addFile(file.name, readFileSync(file.path));
+            }
+            const filename = `${Date.now()}_${user}_aggregated.zip`
+            const destinationFilename = `submitted/${filename}`;
+            const savedPath = `${process.env.NEXT_PUBLIC_UPLOAD_DIR}/${destinationFilename}`
+            zip.writeZip(savedPath);
+            const { size } = statSync(savedPath);
+            const submission: Submission = {
+              stored_name: destinationFilename,
+              upload_name: filename,
+              assignment_config_id: parseInt(fields.assignmentConfigId, 10),
+              size,
+              checksum: sha256File(savedPath),
+              user_id: parseInt(user, 10)
+            }
+            await submit(req.headers.cookie, submission);
+            res.json({
+              status: 'success'
+            });
+          } else {
+            if(files.hash!==fields[`checksum;${files.name}`]) {
               return res.status(500).json({
                 status: 'error',
-                error: error.message
+                error: 'Checksum mismatched, potential transmission corruption detected'
               });
+            }
+            const destinationFilename = `submitted/${files.lastModifiedDate.getTime()}_${user}_${files.path.replace(`/tmp/`, '')}`
+            const submission: Submission = {
+              stored_name: destinationFilename,
+              upload_name: files.name,
+              assignment_config_id: parseInt(fields.assignmentConfigId, 10),
+              size: files.size,
+              checksum: files.hash,
+              user_id: parseInt(user, 10)
+            }
+            try {
+              copyFile(files.path, `${process.env.NEXT_PUBLIC_UPLOAD_DIR}/${destinationFilename}`, async (err) => {
+                if(!err) {
+                  const {id} = await submit(req.headers.cookie, submission);
+                  return res.json({
+                    status: 'success',
+                    id
+                  });
+                }
+              });
+            } catch (error: any) {
+              if (error.message.includes(`Cannot read property 'createSubmission' of undefined`)) {
+  
+                return res.status(403).json({
+                  status: 'error',
+                  error: 'Timeline misalignment for requested submission'
+                })
+              }
+              else {
+                return res.status(500).json({
+                  status: 'error',
+                  error: error.message
+                });
+              }
             }
           }
         }
+      } catch (error: any) {
+        res.status(500).json({
+          status: 'error',
+          error: error.message
+        })
       }
     });
   } catch (error: any) {
