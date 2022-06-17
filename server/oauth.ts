c7acad68c81e7bb4fc54dcb7525562df510f8d49
Kristopher <root@sandb0x.io>
2022-06-17 22:55:15 +0800

    added cookie for domain

--- server/oauth.ts
+++ server/oauth.ts
@@ -64,12 +64,13 @@ const config: ConfigParams = {
       // @ts-ignore
       req.appSession!.userIdentity = additionalUserClaims.data;
       const { email, name } = additionalUserClaims.data;
-      const itsc = email.split('@')[0];
+      const [itsc, domain] = email.split('@');
       const firstName = name.substring(0, name.lastIndexOf(' '));
       const lastName = name.substring(name.lastIndexOf(' ')+1, name.length);
       const { userId, semesterId } = await getUserData(itsc, `${lastName}, ${firstName}`);
       res.cookie('semester', semesterId, { maxAge: SESSION_VALID_FOR, httpOnly: false, domain: `.${process.env.HOSTNAME}` });
       res.cookie('user', userId, { maxAge: SESSION_VALID_FOR, httpOnly: false, domain: `.${process.env.HOSTNAME}` });
+      res.cookie('domain', domain, { maxAge: SESSION_VALID_FOR, httpOnly: false, domain: `.${process.env.HOSTNAME}` });
       res.cookie(
         'client',
         process.env.OIDC_CLIENT_ID,
